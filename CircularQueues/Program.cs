ddcc4230342ac9086b4b13e44d3239de389a09e1
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-12-12 16:36:10 +0700

    create class program

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -9,10 +9,9 @@ namespace CircularQueues
 {
     class Program
     {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
     }
 }
