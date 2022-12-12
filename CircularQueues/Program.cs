a385f50a8127ac54e6fe201bbd9e5bba37094c1f
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-12-12 16:39:41 +0700

    crate method for public program

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -12,6 +12,15 @@ namespace CircularQueues
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
 
+        public Program()
+        {
+            /*Intializing the values of the variables REAR and FRONT to -1 to indicate 
+           * the queue is intially empty. */
+            FRONT = -1;
+            REAR = -1;
+        }
+
+        
 
     }
 }
